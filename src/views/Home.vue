8de1a883e63c0db152f28893e45710f886f5bfce
thejouns <56878512+thejouns@users.noreply.github.com>
2021-09-08 20:43:47 +0200

    several updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="wrapper">
-    <div class="bg-img"></div>
-    <div class="home">
-      <div class="home-txt">
-        <h1>Jouns</h1>
-        <h2>
-          Hey! Ich bin Jonas und mache Videos,<br>
-          Bilder, Webstuff und so...
-        </h2>
+    <article class="home-cover">
+      <video playsinline autoplay muted loop id="video">
+        <source src="@/assets/vid/jouns_background.mp4" type="video/mp4" />
+      </video>
+    </article>
+    <div class="content">
+      <div class="home-links">
+        <router-link id="hlink" to="about">about</router-link>
+        <a id="hlink" target="_blank" href="https://ocular.ch">work</a>
+        <a id="hlink" href="mailto:jonas@ocular.ch">contact</a>
       </div>
       <div class="cta-btns">
         <insta-blob></insta-blob>
@@ -18,61 +20,45 @@
 </template>
 
 <script>
-import InstaBlob from "@/components/InstaBlob.vue";
-import OcularBlob from "@/components/OcularBlob.vue";
-export default {
-  components: {
-    InstaBlob,
-    OcularBlob,
-  },
-};
+export default {};
 </script>
 <style lang="scss">
-.bg-img {
-  background-image: url("~@/assets/img/jouns.jpg");
-  background-size: 190%;
-  background-position: center;
-  opacity: 0.16;
-  height: 100vh;
-  width: 100%;
-  position: absolute;
-  z-index: 0;
-}
-.home {
-  height: 100vh;
+.wrapper {
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  z-index: 1;
-  margin: 0 8vw 0 8vw;
-  .home-txt {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: 65vh;
-    z-index: 1;
-  }
-  .cta-btns {
+  padding: 0;
+  .home-cover {
+    min-height: 100vh;
     width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-  }
-}
-
-@media only screen and (min-width: 1024px) {
-  .bg-img {
-    background-size: 100%;
-    background-position: center;
-  }
-  .home {
-    align-items: left;
-    .home-txt{
-      height: 80vh;
+    background-size: cover;
+    #video {
+      width: 100%;
+      height: 100vh;
+      left: 0;
+      position: absolute;
+      object-fit: cover;
+      object-position: center;
+      z-index: 0;
     }
-    .cta-btns {
-      width: 30%;
+  }
+  .content {
+    margin-top: 10vh;
+    height: 90vh;
+    min-width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    z-index: 1;
+    .home-links {
+      width: 80vw;
+      display: flex;
+      justify-content: space-around;
+      #hlink {
+        color: white;
+        text-decoration: none;
+        mix-blend-mode:overlay;
+        font-size: 12rem;
+      }
     }
   }
 }
