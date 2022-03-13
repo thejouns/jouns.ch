5ba4ab17dbfc5c57eeabd5f054900c3951484c0f
thejouns <56878512+thejouns@users.noreply.github.com>
2022-03-13 18:04:11 +0100

    feat: new webdesign and layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="wrapper">
-    <article class="home-cover">
-      <video playsinline autoplay muted loop id="video">
-        <source src="@/assets/vid/jouns_background.mp4" type="video/mp4" />
-      </video>
-    </article>
+    <article class="home-cover"></article>
     <div class="content-wrapper">
-      <header>
-        <h2 class="jouns">jouns</h2>
-      </header>
+      <div class="home-title">
+        <h1 class="jouns">jouns</h1>
+        <a target="_blank" href="https://instagram.com/joouuns">instagram</a>
+      </div>
       <div class="home-links">
-        <router-link id="hlink" to="about">about</router-link>
-        <a id="hlink" target="_blank" href="https://ocular.ch">work</a>
-        <a id="hlink" href="mailto:jonas@ocular.ch">contact</a>
+        <router-link class="routes" id="hlink" to="shots">shots</router-link>
+        <a class="routes" id="hlink" target="_blank" href="https://ocular.ch/filme"
+          >projects</a
+        >
       </div>
     </div>
+    <footer><p>© 2022 JOUNS</p></footer>
   </div>
 </template>
 
@@ -25,59 +24,63 @@ export default {};
 .wrapper {
   display: flex;
   padding: 0;
-  .home-cover {
-    min-height: 100vh;
-    width: 100%;
-    background-size: cover;
-    #video {
-      width: 100%;
-      height: 100vh;
-      left: 0;
-      top: 0;
-      bottom: 0;
-      position: absolute;
-      object-fit: cover;
-      object-position: center;
-      z-index: 0;
+  margin: 0;
+  background-image: url(~@/assets/img/bg.jpg);
+  background-position-x: center;
+  background-position-y: 38%;
+  background-size: cover;
+}
+.content-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20vh;
+  height: 100vh;
+  flex-direction: row;
+  .home-title {
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+    margin-left: 7vw;
+    width: 40vw;
+    .jouns {
+      z-index: 1;
+      margin: 0;
     }
   }
-  .content-wrapper {
-    display: flex;
-    justify-content: center;
+}
+.home-links {
+  min-width: 10vw;
+  width: 50vw;
+  height: 20vh;
+  display: flex;
+  flex-direction: column;
+  align-items: right;
+  text-align: right;
+  padding-right: 40vw;
+  justify-content: space-around;
+}
+@media only screen and (max-width: 1024px) {
+  .wrapper {
+    background-position: center;
     flex-direction: column;
-    header {
-      .jouns {
-        font-size: 3rem;
-        font-family: eurostile-extended, sans-serif;
-        font-weight: 900;
-        letter-spacing: 0.2rem;
-        font-style: normal;
-        z-index: 1;
-        color: #fff;
-        margin-left: 7vw;
-      }
-    }
-    .home-links {
-      min-width: 100%;
+    .content-wrapper {
       width: 100vw;
-      height: 90vh;
-      display: flex;
-      align-items: center;
-      justify-content: space-around;
-      a {
-        button:focus {
-          outline: none;
+      height: 100vh;
+      flex-direction: column;
+      justify-content: start;
+      .home-title {
+        padding: 2rem;
+        height: 50vh;
+        width: 100vw;
+        .jouns {
+          margin: 0;
         }
-        color: white;
-        text-decoration: none;
-        font-size: 12rem;
-        z-index: 1;
-        mix-blend-mode: difference;
-        filter: brightness(100%);
-        &:hover {
-          mix-blend-mode: hue;
-          color: rgb(81, 176, 214);
-        }
+      }
+      .home-links {
+        margin: 10vh 7vw 10vh 0;
+        width: 100vw;
+        padding: 0;
       }
     }
   }
