18e5f5627e3ba29800a447065a07e2dbce5ef342
Jonas Kunz <Jonas.Kunz1@swisscom.com>
2023-07-26 09:02:49 +0200

    fix: no overlap pb with header

--- src/components/AboutContent.vue
+++ src/components/AboutContent.vue
@@ -1,20 +1,28 @@
 <template>
+
   <div class="about-content">
+
     <div class="pb"><img src="@/assets/img/pb.jpg" alt="" /></div>
+
     <div class="bio">
+
       <h3>JOUNS</h3>
+
       <p>
-        Jonas Kunz, a.k.a. Jouns, is an up-and-coming director and director of
-        photography (DP) based in Bern. He picked up his first camera at an
-        early age to film and photograph everything that caught his eye. In
-        2020, Jonas founded the "Ocular Collective" (ocular.ch) together with
-        like-minded individuals Andrin Schranz and Cedric Ribi. Since then,
-        Jonas has already completed numerous music videos, documentaries, and
-        other types of short films.
+        Jonas Kunz, a.k.a. Jouns, is an up-and-coming director and director of photography
+        (DP) based in Bern. He picked up his first camera at an early age to film
+        and photograph everything that caught his eye. In 2020, Jonas founded the
+        "Ocular Collective" (<a target="_blank" href="https://ocular.ch">ocular.ch</a>)
+        together with like-minded individuals Andrin Schranz and Cedric Ribi.
+        Since then, Jonas has already completed numerous music videos, documentaries,
+        and other types of short films.
       </p>
+
       <div class="sm-cta">
-        <a class="icon" href="https://instagram.com/joouuns" target="_blank"
-          ><svg
+
+        <a class="icon" href="https://instagram.com/joouuns" target="_blank">
+
+          <svg
             id="Gruppe_404"
             data-name="Gruppe 404"
             xmlns="http://www.w3.org/2000/svg"
@@ -22,6 +30,7 @@
             height="35.2"
             viewBox="0 0 35.25 35.25"
           >
+
             <path
               id="Pfad_653"
               data-name="Pfad 653"
@@ -29,6 +38,7 @@
               transform="translate(-81.534 -81.534)"
               fill="#f5f4f3"
             />
+
             <path
               id="Pfad_654"
               data-name="Pfad 654"
@@ -36,31 +46,43 @@
               transform="translate(-129.196 -129.196)"
               fill="#f5f4f3"
             />
+
             <path
               id="Pfad_655"
               data-name="Pfad 655"
               d="M17.627,0A17.627,17.627,0,0,0,0,17.627c.928,23.382,34.329,23.375,35.254,0A17.627,17.627,0,0,0,17.627,0Zm9.5,23.033a4.1,4.1,0,0,1-4.1,4.1H12.221a4.1,4.1,0,0,1-4.1-4.1V12.221a4.1,4.1,0,0,1,4.1-4.1H23.033a4.1,4.1,0,0,1,4.1,4.1Z"
               fill="#f5f4f3"
             />
+
           </svg>
+
         </a>
+
         <a class="icon" href="mailto:info@ocular.ch" target="_blank">
+
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="32.25"
             height="32.25"
             viewBox="0 0 32.25 32.25"
           >
+
             <path
               id="mail"
               d="M16.125,0A16.125,16.125,0,1,0,32.25,16.125,16.114,16.114,0,0,0,16.125,0Zm7.812,21.734H9.815a1.9,1.9,0,0,1-1.9-1.9V12.519a1.818,1.818,0,0,1,.6-1.4h0a1.9,1.9,0,0,1,1.3-.5h12.72a2.109,2.109,0,0,1,1.3.5h0a1.977,1.977,0,0,1,.6,1.4v4.507a.5.5,0,1,1-1,0V12.519a.6.6,0,0,0-.1-.4l-6.21,4.707a1.819,1.819,0,0,1-2,0l-6.21-4.707c-.1.1-.1.2-.1.4v7.311a.9.9,0,0,0,.9.9H23.837a.473.473,0,0,1,.5.5C24.338,21.533,24.137,21.834,23.937,21.734Zm-8.213-5.609L9.815,11.618h12.62l-5.909,4.507A.606.606,0,0,1,15.724,16.125Z"
               fill="#ebebeb"
             />
+
           </svg>
+
         </a>
+
       </div>
+
     </div>
+
   </div>
+
 </template>
 
 <script>
@@ -103,10 +125,12 @@ export default {};
 }
 @media only screen and (max-width: 1024px) {
   .about-content {
+    padding-top: 12vh;
+    height: 88vh;
     flex-direction: column;
     .pb {
       img {
-        height: 60vw;
+        height: 45vw;
       }
     }
     .bio {
@@ -126,3 +150,4 @@ export default {};
   }
 }
 </style>
+
