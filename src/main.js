0f5a663cb3338da140ede4f188121641434106a2
thejouns <56878512+thejouns@users.noreply.github.com>
2024-02-06 20:29:19 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -3,14 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import titleMixin from './mixins/titleMixin';
-import VueSmoothScroll from "vue2-smooth-scroll";
 
 Vue.mixin(titleMixin)
-
-Vue.use(VueSmoothScroll, {
-  duration: 1000,
-  updateHistory: false
-});
 
 Vue.config.productionTip = false;
 
